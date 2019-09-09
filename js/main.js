91252c4f38818d82693db136901a2ed1bd69abe0
Stackbit <projects@stackbit.com>
2019-09-09 03:29:14 +0000

    Stackbit: Updated theme to version 39f8dcd

--- js/main.js
+++ js/main.js
@@ -1,19 +1,4 @@
 
-//service worker
-if ("serviceWorker" in navigator) {
-  if (navigator.serviceWorker.controller) {
-    console.log("An active service worker found, no need to register");
-  } else {
-    // Register the service worker
-    navigator.serviceWorker
-      .register("/serviceworker.js", {
-        scope: "./"
-      })
-      .then(function (reg) {
-        console.log("Service worker has been registered for scope: " + reg.scope);
-      });
-  }
-}
 
 /*!
  * modernizr v3.6.0
