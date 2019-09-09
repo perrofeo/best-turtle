71686cb570f5a3a263184ba4708539a1ce89e9bd
perrofeo <38986663+perrofeo@users.noreply.github.com>
2019-09-08 21:32:58 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -24,21 +24,6 @@
  * of control over the experience.
 */
 
-//service worker
-if ("serviceWorker" in navigator) {
-  if (navigator.serviceWorker.controller) {
-    console.log("An active service worker found, no need to register");
-  } else {
-    // Register the service worker
-    navigator.serviceWorker
-      .register("/serviceworker.js", {
-        scope: "./"
-      })
-      .then(function (reg) {
-        console.log("Service worker has been registered for scope: " + reg.scope);
-      });
-  }
-}
 
 ;(function(window, document, undefined){
   var classes = [];
